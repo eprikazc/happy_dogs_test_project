e686d6f3dfcc88a3d427c014c76329bf57b7898f
Eugene Prikazchikov <eprikazc@gmail.com>
2021-05-28 11:35:20 +0300

    Allow to override SECRET_KEY with env variable

--- happy_dogs/settings.py
+++ happy_dogs/settings.py
@@ -22,7 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=d5%e)=wn8+n%^p4a(k=4dsfv!g3$y9ynjgeqmeyoi0(n58f0t'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-=d5%e)=wn8+n%^p4a(k=4dsfv!g3$y9ynjgeqmeyoi0(n58f0t',
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "1"
